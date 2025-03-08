16b2df68ccf04584d77c1e8019e4fc9a1ff715fb
ASG-SHOP-Team <farhan.fardid@asgshop.ai>
2025-03-08 12:27:57 +0600

    Initial commit setup

---
+++ ejsProject/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("EJS Server");
+});
+
+app.listen(port, () => {
+  console.log("EJS practice project Server is up and Running");
+});
